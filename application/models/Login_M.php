0c839eaf90502e62311e8d3a2a8ace0a17c896a5
Henri0018 <rieltouny@gmail.com>
2023-07-10 11:03:56 +0300

    loginModel

--- application/models/Login_M.php
+++ application/models/Login_M.php
@@ -6,6 +6,18 @@ class Login_M extends CI_Model {
 
     //FONCTION POUR LES REQUETES SQL
 
+
+    // fonction login
+    public function login($email='', $password='',$nom='',$genre='',$taille='',$poids=''){
+        $sql='SELECT*FROM users WHERE email=%s and password=%s';
+        $query=$this->db->query($sql);
+        return $query->result_array();
+    }
+
+    
+   
+
+
 }
      
 
