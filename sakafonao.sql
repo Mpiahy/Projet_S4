9199408804e8f8a58e8c393f9c1f1372883d5568
Henri0018 <rieltouny@gmail.com>
2023-07-10 12:54:06 +0300

    Update sakafonao.sql

--- sakafonao.sql
+++ sakafonao.sql
@@ -10,7 +10,7 @@ use sakafonao;
      genre varchar(30),
      taille int,
      poids int
- )engine = innoDB;
+ );
  INSERT INTO users (id,email,password,nom,genre,taille,poids) VALUES (,'mana@gmail.com','1234','manasoa','masculin',150,70);
  INSERT INTO users (id,email,password,nom,genre,taille,poids) VALUES (,'mom@gmail.com','1444','mpiahy','masculin' , 150,72);
  INSERT INTO users (id,email,password,nom,genre,taille,poids) VALUES (,'aina@gmail.com','1122','aina','masculin',170,80);
