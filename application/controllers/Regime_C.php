8dbd91e7c6798a6ea87cd6a44ee0dfac2e8ef74e
Henri0018 <rieltouny@gmail.com>
2023-07-10 15:48:24 +0300

    admin

---
+++ application/controllers/Regime_C.php
@@ -0,0 +1,36 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Regime_C extends CI_Controller {
+
+    public function __construct() {
+		parent::__construct();
+		$this->load->model('Regime_M');
+		
+	}
+
+    //supprimer regime
+    public function delete($id){
+        $data=array();
+        $data['suppprimer']=$this->Regime_M->supprimer($id);
+        $this->load->view();
+    }
+
+    //ajouter regime
+
+    public function ajouter(){
+        $ajout=$this->Regime_M->ajoute($this->input->post('nom_regime'),$this->input->post('prix'),$this->input->post('duree'));
+        $data['ajoute']=$ajout;
+    }
+
+    // modifier redime
+
+    public function modifier(){
+        $modife=$this->Regime_M->modifier($this->input->post('nom_regime'),$this->input->post('prix'),$this->input->post('duree'));
+        $data['modifier']=$modife;
+        
+
+    }
+
+}
--- application/models/Regime_M.php
+++ application/models/Regime_M.php
@@ -11,6 +11,25 @@ class Regime_M extends CI_Model {
     return $query->result_array();
 
  }
+  //supprimer regime 
+  public function supprimer($id){
+   $sql='DELETE FROM regime where id=%s';
+   $sql=sprintf($sql,$this->db->escape($id));
+   $query=$this->db->query($sql);
+  }
 
+  //modifier regime
+  public function modife($nom_regime,$prix,$duree){
+   $sql='UPDATE regime set id=%s';
+   $sql=sprintf($sql,$this->db->escape($nom_regime),$this->db->escape($prix),$this->db->escape($duree));
+   $query=$this->db->query($sql);
+  }
 
+  //ajouter regime
+  public function ajoute($nom_regime='',$prix='',$duree=''){
+   $sql='INSERT INTO regime(nom_regime,prix,duree) VALUES (%s,%s,%s)';
+   $sql=sprintf($sql,$this->db->escape($nom_regime),$this->db->escape($prix),$this->db->escape($duree));
+   $query=$this->db->query($sql);
+
+  }
 }
