5708be7d0e2266bd05354e3d6d373f6eec7360a8
Henri0018 <rieltouny@gmail.com>
2023-07-10 11:47:17 +0300

    Update sakafonao.sql

--- sakafonao.sql
+++ sakafonao.sql
@@ -17,3 +17,29 @@ use sakafonao;
 
 drop database sakafonao; 
 
+create table login(
+    id int auto_increment primary key,
+    email varchar(50),
+    password varchar(50)
+);
+
+INSERT INTO login(id,email,password) VALUES (,'mana@gmail.com','1234');
+INSERT INTO login(id,email,password) VALUES (,'mom@gmail.com','1444');
+
+create table regime(
+    id int auto_increment primary key,
+    nom_regime varchar(50),
+    prix int ,
+    duree date
+);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'LEGUME',2000,);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'PROTEINE',4000,);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'FRUITS',2000,);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'GLUCIDE',1000,);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'NOIX',2000,);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'Epinard',2000,);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'Poissons gras',4000,);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'Volaille',2000,);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'Avocats',500,);
+INSERT INTO regime(id,nom_regime,prix,duree) VALUES (,'Viande Rouge',3000,);
+
